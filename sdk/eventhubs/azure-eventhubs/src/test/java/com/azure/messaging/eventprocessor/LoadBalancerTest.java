6b4eeaaaf54d4b1b07556495ed6510423e0c6e22
Srikanta <srnagar@microsoft.com>
2019-08-13 15:51:39 -0700

    Update tests

--- sdk/eventhubs/azure-eventhubs/src/test/java/com/azure/messaging/eventprocessor/LoadBalancerTest.java
+++ sdk/eventhubs/azure-eventhubs/src/test/java/com/azure/messaging/eventprocessor/LoadBalancerTest.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
 import org.junit.Test;
 
 public class LoadBalancerTest {
@@ -15,79 +16,30 @@ public class LoadBalancerTest {
 
         PartitionManager pos = new PartitionManagerImpl();
         Map<String, Ownership> ownedPartitions = new ConcurrentHashMap<>();
-        LoadBalancer loadBalancerA = new LoadBalancer("a", "b", "c", ehPartitions, pos,
+        LoadBalancer loadBalancerA = new LoadBalancer("a", "event-hub-name", "consumer-group", ehPartitions, pos,
             ownedPartitions);
-        LoadBalancer loadBalancerB = new LoadBalancer("b", "b", "c", ehPartitions, pos,
+        LoadBalancer loadBalancerB = new LoadBalancer("b", "event-hub-name", "consumer-group", ehPartitions, pos,
             ownedPartitions);
-        LoadBalancer loadBalancerC = new LoadBalancer("c", "b", "c", ehPartitions, pos,
+        LoadBalancer loadBalancerC = new LoadBalancer("c", "event-hub-name", "consumer-group", ehPartitions, pos,
             ownedPartitions);
-        LoadBalancer loadBalancerD = new LoadBalancer("d", "b", "c", ehPartitions, pos,
+        LoadBalancer loadBalancerD = new LoadBalancer("d", "event-hub-name", "consumer-group", ehPartitions, pos,
             ownedPartitions);
-        LoadBalancer loadBalancerE = new LoadBalancer("e", "b", "c", ehPartitions, pos,
+        LoadBalancer loadBalancerE = new LoadBalancer("e", "event-hub-name", "consumer-group", ehPartitions, pos,
             ownedPartitions);
-        LoadBalancer loadBalancerF = new LoadBalancer("f", "b", "c", ehPartitions, pos,
+        LoadBalancer loadBalancerF = new LoadBalancer("f", "event-hub-name", "consumer-group", ehPartitions, pos,
             ownedPartitions);
-        LoadBalancer loadBalancerG = new LoadBalancer("g", "b", "c", ehPartitions, pos,
+        LoadBalancer loadBalancerG = new LoadBalancer("g", "event-hub-name", "consumer-group", ehPartitions, pos,
             ownedPartitions);
 
-
-        loadBalancerA.balance();
-        loadBalancerA.balance();
-        loadBalancerA.balance();
-        loadBalancerA.balance();
-        loadBalancerA.balance();
-        loadBalancerA.balance();
-
-        System.out.println("__________________________________");
-        loadBalancerB.balance();
-        loadBalancerB.balance();
-        loadBalancerB.balance();
-        loadBalancerB.balance();
-        loadBalancerB.balance();
-        loadBalancerB.balance();
-
-        System.out.println("__________________________________");
-        loadBalancerD.balance();
-        loadBalancerD.balance();
-        loadBalancerD.balance();
-        loadBalancerD.balance();
-        loadBalancerD.balance();
-        loadBalancerD.balance();
-
-        System.out.println("__________________________________");
-        loadBalancerE.balance();
-        loadBalancerE.balance();
-        loadBalancerE.balance();
-        loadBalancerE.balance();
-        loadBalancerE.balance();
-        loadBalancerE.balance();
-
-
-        System.out.println("__________________________________");
-        loadBalancerF.balance();
-        loadBalancerF.balance();
-        loadBalancerF.balance();
-        loadBalancerF.balance();
-        loadBalancerF.balance();
-        loadBalancerF.balance();
-
-
-        System.out.println("__________________________________");
-        loadBalancerG.balance();
-        loadBalancerG.balance();
-        loadBalancerG.balance();
-        loadBalancerG.balance();
-        loadBalancerG.balance();
-        loadBalancerG.balance();
-
-        System.out.println("__________________________________");
-        loadBalancerC.balance();
-        loadBalancerC.balance();
-        loadBalancerC.balance();
-        loadBalancerC.balance();
-        loadBalancerC.balance();
-        loadBalancerC.balance();
-
+        // run each load balancer 6 times
+        IntStream.range(0, 6). forEach(unused -> loadBalancerA.balance());
+        IntStream.range(0, 6). forEach(unused -> loadBalancerB.balance());
+        IntStream.range(0, 6). forEach(unused -> loadBalancerC.balance());
+        IntStream.range(0, 6). forEach(unused -> loadBalancerD.balance());
+        IntStream.range(0, 6). forEach(unused -> loadBalancerE.balance());
+        IntStream.range(0, 6). forEach(unused -> loadBalancerF.balance());
+        IntStream.range(0, 6). forEach(unused -> loadBalancerG.balance());
+        IntStream.range(0, 6). forEach(unused -> loadBalancerC.balance());
     }
 
 }
