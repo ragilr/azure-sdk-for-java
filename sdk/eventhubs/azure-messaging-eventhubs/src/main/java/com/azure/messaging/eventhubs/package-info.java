80a7eea8351c67133b5f9fc2ec4cf007b67eb2fe
Connie <conniey@microsoft.com>
2019-08-15 08:39:54 -0700

    Updating package-info to include sync API

--- sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/package-info.java
+++ sdk/eventhubs/azure-messaging-eventhubs/src/main/java/com/azure/messaging/eventhubs/package-info.java
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 /**
- * Package containing classes for creating a {@link com.azure.messaging.eventhubs.EventHubAsyncClient} to
- * perform operations on Azure Event Hubs.
+ * Package containing classes for creating a {@link com.azure.messaging.eventhubs.EventHubAsyncClient} or
+ * {@link com.azure.messaging.eventhubs.EventHubClient} to perform operations on Azure Event Hubs.
  */
 package com.azure.messaging.eventhubs;
